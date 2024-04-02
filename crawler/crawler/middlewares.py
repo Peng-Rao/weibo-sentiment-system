cacc7271a65553128d25371022b3a48c4eb05e14
Peng Rao <76681491+Raopend@users.noreply.github.com>
2024-04-02 13:27:17 +0800

    增加 README.md

--- crawler/crawler/middlewares.py
+++ crawler/crawler/middlewares.py
@@ -1,18 +1,7 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 from scrapy import signals
-
-# useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
 
 
 class CrawlerSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
 
     @classmethod
     def from_crawler(cls, crawler):
--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,11 +1,4 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 import pymongo
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 from classifier.model import LstmClassifier
